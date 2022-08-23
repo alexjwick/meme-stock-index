fdc5bbbcc56d7ceb209deb2648aef3975518564a
alexjwick <alexjwick03@gmail.com>
2022-08-23 15:42:36 -0400

    added percent change

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const api_url = (after, before) => `https://api.pushshift.io/reddit/submission/s
 
 const tickers_blacklist = ["WSB", "HODL", "APES", "BUY", "YOLO", "FUD", "YOU", "THE", "NOT", "HOLD", "APE", "ALL", "TLDR", "LFG", "SEC", "FUCK", "MSM", "FOMO", "BABY", "HALT", "STFU", "IRA", "SELL", "WISH", "FROM", "BLUE", "AND", "ARE", "USD", "FOR", "WTF", "WHAT", "PSA", "CEO"];
 
-let tickers_ranking = new Map();
+let tickers_mentions = new Map();
+let tickers_mentions_prev = new Map();
 
 const before = Math.floor(Date.now() / 1000);
-const after = before - 86400; // 24 hours ago
+const after = before - 172800; // 48 hours ago
+const time_split = before - 86400; // 24 hours ago
 
 getApiData(api_url(after, before));
 
@@ -26,7 +28,7 @@ async function getApiData(url) {
         getApiData(api_url(data.data.slice(-1)[0].created_utc + 1, before));
     } else {
         hideLoader();
-        show(tickers_ranking);
+        showRankings();
     }
 }
 
@@ -35,12 +37,18 @@ function rankTickers(data) {
     for (let r of data.data) {
         let content = r.title + " " + r.selftext;
         let tickers = new Set(content.match(ticker_regex));
+        let map;
+        if (r.created_utc > time_split) {
+            map = tickers_mentions;
+        } else {
+            map = tickers_mentions_prev;
+        }
         for (let t of tickers) {
             if (!tickers_blacklist.includes(t)) {
-                if (tickers_ranking.has(t)) {
-                    tickers_ranking.set(t, tickers_ranking.get(t) + 1);
+                if (map.has(t)) {
+                    map.set(t, map.get(t) + 1);
                 } else {
-                    tickers_ranking.set(t, 1);
+                    map.set(t, 1);
                 }
             }
         }
@@ -48,7 +56,7 @@ function rankTickers(data) {
 }
 
 function updatePercentage(current_timestamp) {
-    let percent = 100 - Math.floor((before - current_timestamp) / 86400 * 100);
+    let percent = 100 - Math.floor((before - current_timestamp) / 172800 * 100);
     document.getElementById("percentage").innerHTML = percent;
 }
 
@@ -56,21 +64,23 @@ function hideLoader() {
     document.getElementById('loader').style.display = 'none';
 }
 
-function show(tickers) {
-    let tickers_sorted = new Map([...tickers.entries()].sort((a, b) => b[1] - a[1]));
+function showRankings() {
+    let tickers_ranking = new Map([...tickers_mentions.entries()].sort((a, b) => b[1] - a[1]));
     let tab =
         `<tr>
           <th>Rank</th>
           <th>Ticker</th>
           <th>Mentions</th>
+          <th>% change</th>
          </tr>`;
 
     let rank = 1;
-    for (let t of tickers_sorted.keys()) {
+    for (let t of tickers_ranking.keys()) {
         tab += `<tr>
     <td>${rank}</td>
     <td>${t}</td>
-    <td>${tickers_sorted.get(t)}</td>       
+    <td>${tickers_ranking.get(t)}</td>
+    <td class="${getPercentChangeColor(t)}">${getPercentChangeFromYesterday(t)}%</td>       
 </tr>`;
         if (rank === 20) {
             break;
@@ -79,4 +89,30 @@ function show(tickers) {
     }
 
     document.getElementById("tickers").innerHTML = tab;
+}
+
+function getPercentChangeFromYesterday(ticker) {
+    if (tickers_mentions_prev.has(ticker)) {
+        const percent_change = Math.floor(tickers_mentions.get(ticker) / tickers_mentions_prev.get(ticker) * 100) - 100;
+        if (percent_change < 0) {
+            return percent_change;
+        } else {
+            return "+" + percent_change;
+        }
+    } else {
+        return "+&infin;";
+    }
+}
+
+function getPercentChangeColor(ticker) {
+    if (tickers_mentions_prev.has(ticker)) {
+        const percent_change = Math.floor(tickers_mentions.get(ticker) / tickers_mentions_prev.get(ticker) * 100) - 100;
+        if (percent_change < 0) {
+            return "red";
+        } else {
+            return "green";
+        }
+    } else {
+        return "green";
+    }
 }
